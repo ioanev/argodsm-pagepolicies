8b0933b5b1e2a1854a5c75abfd6135cf32b4a7a3
johnpc <ioannis.anevlavis.8003@student.uu.se>
2020-03-21 20:01:39 +0100

    Useless changes.

--- changes.sh
+++ changes.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 scp -r ./src ioan8003@pc-davkl518.it.uu.se:./argodsm
-scp -r ./CMakeLists.txt ioan8003@pc-davkl518.it.uu.se:./argodsm
-scp -r ./src/CMakeLists.txt ioan8003@pc-davkl518.it.uu.se:./argodsm/src
-
 #scp -r ./c++ ioan8003@rackham.uppmax.uu.se:./argodsm
